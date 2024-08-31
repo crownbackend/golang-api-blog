0d249af907e63f25f73de51d45b5df38b3aa0e84
Crownbackend <crownbackend@gmail.com>
2024-09-01 00:47:52 +0200

    add gin and air hot reload

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/crownbackend/golang-api-blog/models"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	user := models.User{
-		Id:       1,
-		Email:    "john@doe.fr",
-		Password: "1234",
-	}
-	fmt.Printf("User: %+v \n", user)
+	r := gin.Default()
+
+	r.GET("/", home)
+
+	r.Run("localhost:8000")
+}
+
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"data": "testo"})
 }
--- models/user.go
+++ models/user.go
@@ -1,7 +1,9 @@
 package models
 
 type User struct {
-	Id       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Id        int    `json:"id"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"firstname"`
 }
